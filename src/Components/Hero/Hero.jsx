35a3fbb9bc0769e317d2d78a37b161be745184a7
Johnnysky <vanpasie20@gmail.com>
2026-01-22 04:33:53 -0800

    completing the navbar and hero components

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-
+    <div className='hero container'>
+        <div className="hero-text">
+          <h1>We Ensure Better Education For A Better World</h1>
+          <p>StudySphere is a platform dedicated to providing students with the knowledge, skills,
+            experience, high-quality educational resources and tools
+            to help students achieve their academic goals.</p>
+            <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
+        </div>
     </div>
   )
 }
